e98250bfb3fb7c90c4b698de8a4ff8308ea1b204
indra puluraga <indralolx@gmail.com>
2024-04-09 17:55:39 +0800

    add some audio/bgm

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ const container = document.querySelector(".container");
 btn.addEventListener("click", function () {
    console.log(container);
    container.style.display = "block";
+   const music = new Audio("./EidHappy.mp3");
    btn.style.display = "none";
+   music.play();
+   music.loop();
 });
